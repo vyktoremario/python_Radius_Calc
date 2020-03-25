b85bfb961093976201d78e2db6dacc54c88a51e8
vyktoremario <umehvictormario@gmail.com>
2020-03-25 23:32:00 +0100

    starting the project

---
+++ areaCalculator.py
@@ -0,0 +1,13 @@
+print ('Area of a Circle Calculator!!')
+
+# Area of a cirlce is PI*Radius*Radius, PI is constant
+PI=3.14
+
+#Let user enter the radius value
+radius = float(input(' Please Enter the radius of the circle: '))
+
+# calculating the Area of circle, using the formular
+area= PI * radius * radius
+
+# Printing the value of Area
+print('Area: %.2f' %area)
